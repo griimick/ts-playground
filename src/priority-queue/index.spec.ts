33667f8b1e59fdabd5fa84f9dcc17786f62f0bd1
Soumik Pradhan <soumik@antarx.tech>
2022-02-07 05:47:16 +0530

    Add test cases

--- src/priority-queue/index.spec.ts
+++ src/priority-queue/index.spec.ts
@@ -1,11 +1,16 @@
+import { PriorityQueue } from './index';
 import { expect } from 'chai';
 import 'mocha';
 
-describe('Hello function', () => {
-
-	it('should return hello world', () => {
-		let result = 'Hello world!';
-		expect(result).to.equal('Hello world!');
+describe('test cases for priority queue', () => {
+	it('sort a random array', () => {
+		let input = Array.from({ length: 10 }, () => Math.floor(Math.random() * 10));
+		let output = input.sort((a: number ,b: number): number => a - b);
+		let queue = new PriorityQueue({
+			comparatorFn: (a: number,b: number): number => a - b,
+			initialValues: input,
+		});
+		expect(queue.heapsort()).to.deep.equal(output);
 	});
 
 });
